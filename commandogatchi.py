273ad756dbfc3809e0a137e7667ad90090c265bd
Orinax <chaunceypelton@gmail.com>
2022-08-08 21:32:24 +0700

    Add more to the main class

--- commandogatchi.py
+++ commandogatchi.py
@@ -1,4 +1,11 @@
 class Commandogatchi:
-    def __init__(self, name, age):
+    def __init__(self, name, age, hunger=0, happiness=0, health=0,
+                growth=0, weight=0, discipline=0):
         self.name = name
         self.age = age
+        self.hunger = hunger
+        self.happiness = happiness
+        self.health = health
+        self.growth = growth
+        self.weight = weight
+        self.discipline = discipline
